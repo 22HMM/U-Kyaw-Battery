81a9f5f5d6b38efdf93978d7cdfb972d77cd7c50
Khant Loon Thu <khantloonthu66@gmail.com>
2025-05-06 15:45:12 +0630

    updated - better accessibility on services page!

--- public/services.php
+++ public/services.php
@@ -1,6 +1,6 @@
 <?php include_once __DIR__ . '/partials/header.php'; ?>
 <!-- Banner -->
-<section class="bg-[#FFFCFF] mt-[60px] lg:mt-[70px] xl:mt-[90px]">
+<header class="bg-[#FFFCFF] mt-[60px] lg:mt-[70px] xl:mt-[90px]">
     <div class="relative grid theme-container md:grid-cols-3">
         <div class="hidden md:block"></div>
         <article class="absolute z-10 flex flex-col w-full max-w-[250px] xs:max-w-[350px] sm:max-w-xl lg:max-w-2xl leading-[120%] gap-6 px-2 transform -translate-y-1/2 md:px-6 left-4 sm:left-6 top-1/2 bg-[#FFFCFF] py-6 sm:py-12 md:py-16 lg:py-24">
@@ -16,10 +16,11 @@
                 width="917"
                 height="685"
                 title="Banner Image of Services Page"
+                fetchpriority="high"
                 loading="eager" />
         </figure>
     </div>
-</section>
+</header>
 
 <!-- Services -->
 <?php
@@ -27,7 +28,7 @@ $services = getServices();
 ?>
 <section class="py-[60px] md:py-[80px] theme-container space-y-[40px] md:space-y-[80px]">
     <?php foreach ($services as $service): ?>
-        <div class="flex flex-col gap-6 bg-white md:items-end md:flex-row md:even:flex-row-reverse" aria-label="Services Section">
+        <div class="flex flex-col gap-6 bg-white md:items-end md:flex-row md:even:flex-row-reverse" aria-label="<?= htmlspecialchars(__("services.services.{$service['key']}.title"), ENT_QUOTES, 'UTF-8') ?>">
             <div class="w-full md:w-1/2">
                 <figure class="overflow-hidden aspect-w-16 aspect-h-10">
                     <img
@@ -37,7 +38,8 @@ $services = getServices();
                         width="644"
                         height="413"
                         title="<?= __("services.services.{$service['key']}.title"); ?>"
-                        loading="<?= $service['key'] == 'service_1' ? 'eager' : 'lazy' ?>" />
+                        loading="<?= $service['key'] == 'service_1' ? 'eager' : 'lazy' ?>"
+                        fetchpriority="<?= $service['key'] == 'service_1' ? 'high' : 'low' ?>" />
                     <figcaption class="sr-only"><?= __("services.services.{$service['key']}.title"); ?></figcaption>
                 </figure>
             </div>
@@ -45,9 +47,9 @@ $services = getServices();
                 <h3 class="capitalize heading-3"><?= __("services.services.{$service['key']}.title"); ?></h3>
                 <p class="content-text text-justify text-[#3E3D3D]"><?= __("services.services.{$service['key']}.description"); ?></p>
                 <div>
-                    <a href="/contact-us" class="text-[#1F1F1F] btn-contact group">
+                    <a href="/contact-us" class="text-[#1F1F1F] btn-arrow group">
                         <span class="font-semibold group-hover:underline underline-offset-4 content-text"><?= __('contact.title') ?></span>
-                        <svg class="btn-contact__icon group-hover:transform group-hover:translate-x-1 group-focus:translate-x-1 group-hover:text-[#0C40C5] group-focus:text-[#0C40C5]"
+                        <svg class="btn-arrow__icon group-hover:transform group-hover:translate-x-1 group-focus:translate-x-1 group-hover:text-[#0C40C5] group-focus:text-[#0C40C5]"
                             aria-hidden="true"
                             focusable="false"
                             width="25"
